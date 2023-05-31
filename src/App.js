541e4794781ab029c43b776d41048adb443e3608
mariana.nunes <mariana.nunes@compasso.com.br>
2023-05-31 20:59:14 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div className="App">
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from "./components/login"
+import Home from "./components/home"
+import Menu from "./components/menu"
+import Cadastro from './components/cadastro'
 
-    </div>
-  );
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/cadastro" element={<Cadastro/>}/>
+        <Route path="/home" element={[<Menu/>, <Home/>]}/>
+      </Routes>
+    </BrowserRouter>
+  )
+  
 }
 
-export default App;
+export default App

65c4ebbc7d96e04e0bb40f840342f97e3fb12320
Guilherme Quirino de Melo <guilhermequirino74@gmail.com>
2023-06-03 01:10:26 -0300

    mais um post

--- src/components/postPage.js
+++ src/components/postPage.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { Post1 } from "./posts/post1";
 import { Post2 } from "./posts/post2";
 import { Post3 } from "./posts/post3";
-
+import { Post4 } from "./posts/post4";
 
 function PostPage(){
   const { id } = useParams()
@@ -13,6 +13,7 @@ function PostPage(){
       {id === '1' && (<Post1/>)}
       {id === '2' && (<Post2/>)}
       {id === '3' && (<Post3/>)}
+      {id === '4' && (<Post4/>)}
     </div>
   );
 };

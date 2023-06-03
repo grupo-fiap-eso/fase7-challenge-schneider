d06974e24f38539bf88fd2e26a2fef2192d7b3fb
Guilherme Quirino de Melo <guilhermequirino74@gmail.com>
2023-06-03 01:09:50 -0300

    fix logo e css

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import Logo from "../components/img/logo2.png"
 
 function Menu() {
     const menu = {
@@ -15,7 +16,7 @@ function Menu() {
     }
     return (
         <nav style={menu}>
-            <a class="navbar-brand logo" href="#"><img src="./imagens/logo2.png" alt="" style={{ height: '35px' }} /></a>
+            <a class="navbar-brand logo" href="#"><img src={Logo} alt="" style={{ height: '35px' }} /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
